a984f0a34c9613e14ca673d31cc0e0248bf58128
Isabella <ikmaki14@gmail.com>
2019-06-24 14:19:06 -0500

    Initial commit

---
+++ emojional/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  emojional
+//
+//  Created by Isabella Maki on 6/24/19.
+//  Copyright © 2019 Isabella Maki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
