a5b034413262a30a4dab5fcbca771a19bdbbc3e2
Isabella <ikmaki14@gmail.com>
2019-06-24 17:20:44 -0500

    Created UI

--- emojional/ViewController.swift
+++ emojional/ViewController.swift
@@ -9,7 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var happy_face: UIImageView!
+    @IBOutlet weak var laughing_face: UIImageView!
+    @IBOutlet weak var expressionless_face: UIImageView!
+    @IBOutlet weak var crying_face: UIImageView!
+    @IBOutlet weak var crazy_face: UIImageView!
+    @IBOutlet weak var cool_face: UIImageView!
+    @IBOutlet weak var angry_face: UIImageView!
+    @IBOutlet weak var poop: UIImageView!
+    
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "happy", message: "You're happy", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
