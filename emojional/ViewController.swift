5ad9a65b358aa39fb0eccacfcb19edca80001a72
Isabella <ikmaki14@gmail.com>
2019-06-25 14:46:42 -0500

    Complete Iteration 3

--- emojional/ViewController.swift
+++ emojional/ViewController.swift
@@ -9,11 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let emojis = ["happy": "😀", "laughing": "😂", "expressionless": "😑", "crying": "😭", "crazy": "😜", "cool": "😎", "angry": "😡", "poop": "💩"]
+    let emojis = ["happy": "happy", "laughing": "laughing", "expressionless": "expressionless", "crying": "crying", "crazy": "crazy", "cool": "cool", "angry": "angry", "poop": "poop"]
+    
+    let customMessages = ["happy": ["It's a great day!", "Happy, happy, happy!", "Woo hoo!"], "laughing": ["Hahahahahaha", "Knock knock...Who's there?...Banana...", "What do you call a sleepy dinosaur?...A dinosnore!"], "expressionless": ["ugh", "blah", "ehh"], "crying": ["Why are you sad?", "Feel better", "Can I cheer you up?"], "crazy": ["AHHHH!", "PARTY TIME!", "🎉🎊🎉🎊🎉🎊"], "cool": ["sup", "I'm chill", "*head nod*"], "angry": ["😤😤😤😤", "ARGHHH!", "Let's take 3 deep breathes..."], "poop": ["💩", "💩💩", "💩💩💩"]]
     
     @IBAction func showMessage(sender: UIButton) {
         let selectedEmotion = sender.titleLabel?.text
-        let alertController = UIAlertController(title: "Special message:", message: emojis[selectedEmotion!], preferredStyle: UIAlertController.Style.alert)
+        let number = Int.random(in: 0 ..< 3)
+        let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[number]
+        let alertController = UIAlertController(title: "Special message:", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
         
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
